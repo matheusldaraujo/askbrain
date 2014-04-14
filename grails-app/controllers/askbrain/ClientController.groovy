e60c9354ef6eeb0f5da8b6184ff8551fa259502b
Matheus Araujo <araujoma@msu.edu>
2014-04-14 16:31:10 -0400

    Add Twitter Bootstrap and favicon.png

--- grails-app/controllers/askbrain/ClientController.groovy
+++ grails-app/controllers/askbrain/ClientController.groovy
@@ -26,7 +26,6 @@ class ClientController {
         println("Client question: \"$params.question\"")
         println(Workflow.list())
         mturkMonitorService.launch(w,params.type=="real",params.iterations as int,Credentials.get(params.credentials as long), params.props as Map)
-
         redirect(action: "loading")
     }
 
